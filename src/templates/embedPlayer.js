1349a139a7cab8507762363c20cd64a2f8735150
FraBr <brault.fra@gmail.com>
2019-11-24 18:30:15 +0100

    test twitter card

---
+++ src/templates/embedPlayer.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Show from "../components/show"
+import { Helmet } from "react-helmet"
+
+export default ({ data }) => {
+  const post = data.markdownRemark
+  console.log(post)
+  return (
+    <html>
+      <Helmet>
+        <meta name="twitter:card" content="player"/>
+        {/* <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" /> */}
+      </Helmet>
+      <Show
+        avec={post.frontmatter.avec}
+        date={post.frontmatter.date}
+        title={post.frontmatter.title}
+        url={post.frontmatter.url}
+        image={post.frontmatter.image}
+        show={post.frontmatter.show}
+        episode={post.frontmatter.episode}
+        subject={post.frontmatter.subject}
+        htmlAst={post.htmlAst}
+      />
+    </html>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      htmlAst
+      frontmatter {
+        status
+        avec
+        date
+        title
+        url
+        image
+        show
+        episode
+        subject
+      }
+    }
+  }
+`
